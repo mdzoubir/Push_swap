ea325f248ee941a08ad2d589f14c422c8583430b
Mohamed zoubir <simozoubir012@gmail.com>
2025-12-17 16:51:18 +0100

    adding the push function pa and pb

--- operations/push.c
+++ operations/push.c
@@ -1,6 +1,29 @@
 #include "../push_swap.h"
 
-void	pb(t_node **stack_a, t_node **stack_b)
+void pa(t_node **stack_a, t_node **stack_b)
 {
+    t_node *tmp;
 
+    if (!stack_b || !*stack_b)
+        return;
+
+    tmp = *stack_b;
+    *stack_b = tmp->next;
+    tmp->next = *stack_a;
+    *stack_a = tmp;
+    write(1, "pa\n", 3);
+}
+
+void pb(t_node **stack_a, t_node **stack_b)
+{
+    t_node *tmp;
+
+    if (!stack_a || !*stack_a)
+        return;
+
+    tmp = *stack_a;
+    *stack_a = tmp->next;
+    tmp->next = *stack_b;
+    *stack_b = tmp;
+    write(1, "pb\n", 3);
 }
