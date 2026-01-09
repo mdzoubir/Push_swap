d6fc6aec3390273ca67de6aa13b1b1bdf3e78ea3
mzoubir <mzoubir@student.1337.ma>
2026-01-09 13:49:23 +0100

    adding norm header in c files

--- sorting/sort_chunk.c
+++ sorting/sort_chunk.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   sort_chunk.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mzoubir <mzoubir@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mzoubir <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 21:12:41 by mzoubir           #+#    #+#             */
-/*   Updated: 2025/12/28 21:12:43 by mzoubir          ###   ########.fr       */
+/*   Updated: 2026/01/09 13:37:59 by mzoubir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void optimal_rotate(t_node **stack, int pos)
+void	optimal_rotate(t_node **stack, int pos)
 {
-	int size;
+	int	size;
 
 	size = stack_size(*stack);
 	if (pos <= size / 2)
@@ -29,86 +29,86 @@ void optimal_rotate(t_node **stack, int pos)
 	}
 }
 
-static int get_chunk_value(t_node *a)
+static int	get_chunk_value(t_node *a)
 {
-    int size;
+	int	size;
 
-    size = stack_size(a);
-    if (size <= 10)
-        return (3);
-    else if (size <= 100)
-        return (5);
-    else
-        return (15);
+	size = stack_size(a);
+	if (size <= 10)
+		return (3);
+	else if (size <= 100)
+		return (5);
+	else
+		return (15);
 }
 
-void    get_closest_node(t_node **a, int max)
+void	get_closest_node(t_node **a, int max)
 {
-    t_node  *tmp;
-    int     first_match;
-    int     last_match;
-    int     best_pos;
+	t_node	*tmp;
+	int		first_match;
+	int		last_match;
+	int		best_pos;
+	int		i;
 
-    tmp = *a;
-    first_match = -1;
-    last_match = -1;
-    
-    int i = 0;
-    while (tmp)
-    {
-        if (tmp->index <= max)
-        {
-            if (first_match == -1)
-                first_match = i;
-            last_match = i;
-        }
-        tmp = tmp->next;
-        i++;
-    }
-    if (first_match != -1)
-    {
-        if (first_match <= (stack_size(*a) - last_match))
-            while (first_match-- > 0)
-                ra(a, 1);
-        else
-        {
-            best_pos = stack_size(*a) - last_match;
-            while (best_pos-- > 0)
-                rra(a, 1);   
-        }   
-    }
+	tmp = *a;
+	first_match = -1;
+	last_match = -1;
+	i = 0;
+	while (tmp)
+	{
+		if (tmp->index <= max)
+		{
+			if (first_match == -1)
+				first_match = i;
+			last_match = i;
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	if (first_match != -1)
+	{
+		if (first_match <= (stack_size(*a) - last_match))
+			while (first_match-- > 0)
+				ra(a, 1);
+		else
+		{
+			best_pos = stack_size(*a) - last_match;
+			while (best_pos-- > 0)
+				rra(a, 1);
+		}
+	}
 }
 
-void push_chunks(t_node **a, t_node **b)
+void	push_chunks(t_node **a, t_node **b)
 {
-    int size;
-    int chunk_size;
-    int i;
+	int	size;
+	int	chunk_size;
+	int	i;
 
-    size = stack_size(*a);
-    chunk_size = size / get_chunk_value(*a);
-    if (size % get_chunk_value(*a))
-        chunk_size++;
-    i = 0;
-    while (*a)
-    {
-        if ((*a)->index <= i)
-        {
-            pb(a, b, 1);
-            rb(b, 1);
-            i++;
-        }
-        else if ((*a)->index <= i + chunk_size)
-        {
-            pb(a, b, 1);
-            i++;
-        }
-        else
-            get_closest_node(a, i + chunk_size);
-    }
+	size = stack_size(*a);
+	chunk_size = size / get_chunk_value(*a);
+	if (size % get_chunk_value(*a))
+		chunk_size++;
+	i = 0;
+	while (*a)
+	{
+		if ((*a)->index <= i)
+		{
+			pb(a, b, 1);
+			rb(b, 1);
+			i++;
+		}
+		else if ((*a)->index <= i + chunk_size)
+		{
+			pb(a, b, 1);
+			i++;
+		}
+		else
+			get_closest_node(a, i + chunk_size);
+	}
 }
 
-static void tmp_push(t_node **a, t_node **b, int max, int pos_prev)
+static void	tmp_push(t_node **a, t_node **b, int max, int pos_prev)
 {
 	optimal_rotate(b, pos_prev);
 	pa(a, b, 1);
@@ -117,13 +117,13 @@ static void tmp_push(t_node **a, t_node **b, int max, int pos_prev)
 	sa(a, 1);
 }
 
-void push_back_to_a(t_node **a, t_node **b)
+void	push_back_to_a(t_node **a, t_node **b)
 {
-	int max;
-	int prev;
-	int pos_max;
-	int pos_prev;
-	int size;
+	int	max;
+	int	prev;
+	int	pos_max;
+	int	pos_prev;
+	int	size;
 
 	while (*b)
 	{
@@ -132,8 +132,8 @@ void push_back_to_a(t_node **a, t_node **b)
 		prev = max - 1;
 		pos_max = get_pos(*b, max);
 		pos_prev = get_pos(*b, prev);
-		if (pos_prev != -1 && get_cost(pos_prev, size) < get_cost(pos_max,
-																  size))
+		if (pos_prev != -1 && get_cost(pos_prev, size)
+			< get_cost(pos_max, size))
 			tmp_push(a, b, max, pos_prev);
 		else
 		{
